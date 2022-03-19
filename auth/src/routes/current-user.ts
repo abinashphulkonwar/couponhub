f29135b8290494fe8e11a5d9f2beb09bba7c6b5d
abinash <abinashphulkonwar1@gmail.com>
2022-03-19 17:15:54 +0530

    add current user api

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,18 @@
 import express from "express";
-
+import jwt from "jsonwebtoken";
 const router = express.Router();
 
 router.get("/currentuser", async (req, res) => {
-  res.json({ currentUser: "hiiiiiiiiii" });
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    const payload = await jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.json({ currentUser: payload });
+  } catch (err) {
+    res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
